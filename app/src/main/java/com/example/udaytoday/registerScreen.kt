69e839786a9d1b6c1d141074ffbc21203c298b0f
CharlesJKK <selrahcjunior@hotmail.com>
2023-03-02 16:32:23 -0300

    feat: adicionando funcionalidade nos botões de voltar para tela de login(already_registered e icon_back)

--- app/src/main/java/com/example/udaytoday/registerScreen.kt
+++ app/src/main/java/com/example/udaytoday/registerScreen.kt
@@ -1,11 +1,41 @@
 package com.example.udaytoday
 
+import android.content.Intent
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.example.udaytoday.databinding.ActivityRegisterScreenBinding
 
 class registerScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_screen)
+        binding = ActivityRegisterScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val iconBack = binding.backBtn
+        val alreadyRegistered = binding.alreadyRegistered
+        alreadyRegistered.paintFlags = alreadyRegistered.paintFlags or Paint.UNDERLINE_TEXT_FLAG;
+
+        val onClickListener = View.OnClickListener { view ->
+            when(view.id){
+                R.id.already_registered -> {
+                    val navigationToMain = Intent(this, MainActivity::class.java)
+                    startActivity(navigationToMain)
+                }
+                R.id.back_btn -> {
+                    val navigationToMain = Intent(this, MainActivity::class.java)
+                    startActivity(navigationToMain)
+                }
+            }
+        }
+
+        iconBack.setOnClickListener(onClickListener)
+        alreadyRegistered.setOnClickListener(onClickListener)
+
     }
 }
