7483a34857d87e74a333942d380dd76a5e3cc674
CharlesJKK <selrahcjunior@hotmail.com>
2023-03-03 19:06:00 -0300

    feat: Adicionando verificação para saber se os campos de nome e senha estão vazios e navegação para tela de home.

--- app/src/main/java/com/example/udaytoday/MainActivity.kt
+++ app/src/main/java/com/example/udaytoday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.udaytoday
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.udaytoday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +18,26 @@ class MainActivity : AppCompatActivity() {
         binding.registerBtn.setOnClickListener {
             val navigationToRegister = Intent(this, registerScreen::class.java)
             startActivity(navigationToRegister)
+        }
+
+        val loginBtn = binding.loginBtn
+
+        loginBtn.setOnClickListener {
+            login()
+        }
+    }
+    private fun login(){
+
+        val usernameField = binding.usernameLogin.text.toString()
+        val passwordField = binding.passwordLogin.text.toString()
+        var errorTxt = binding.emptyFields
+
+        if (usernameField == "" || passwordField == ""){
+            errorTxt.visibility = View.VISIBLE
+            return
+        }else{
+            val navigationToHome = Intent(this, HomeActivity::class.java)
+            startActivity(navigationToHome)
         }
     }
 }
