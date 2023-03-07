f99bcb04c94de44a7946ab367004f4188544163c
CharlesJKK <selrahcjunior@hotmail.com>
2023-03-06 23:01:23 -0300

    feat and fix: Adicionando a bottom navigation ainda incompleta e retirando o antigo xml da HomeActivity pois estava dando erros.

--- app/src/main/java/com/example/udaytoday/HomeActivity.kt
+++ app/src/main/java/com/example/udaytoday/HomeActivity.kt
@@ -2,10 +2,40 @@ package com.example.udaytoday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.udaytoday.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding =  ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        replaceFragmente(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.home -> replaceFragmente(Home())
+                R.id.reminder -> replaceFragmente(Reminder())
+
+                else -> {
+
+                }
+
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragmente(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransition = fragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.frame_layout,fragment)
+        fragmentTransition.commit()
     }
 }
